9f40bafdabe8028dcc1864e73233031695a2fee9
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-20 00:02:07 +0100

    feat: added resuable paystack button

---
+++ components/PaystackButton.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+
+const PaystackButton = ({ email, amount, studentID, type }) => {
+    const [loading, setLoading] = useState(false);
+
+    const handlePayment = async () => {
+        if (!email || !email.includes("@")) {
+            alert("Please provide a valid email.");
+            return;
+        }
+
+        if (!amount || amount <= 0) {
+            alert("Amount must be greater than zero.");
+            return;
+        }
+
+        setLoading(true);
+
+        try {
+            // Call API route
+            const res = await fetch("/api/payment", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({studentID, email, amount, type }),
+            });
+
+            const data = await res.json();
+
+            if (data?.authorization_url) {
+                // Redirect user to Paystack checkout page
+                window.location.href = data.authorization_url;
+            } else {
+                alert("Failed to initialize payment. Try again.");
+            }
+        } catch (err) {
+            console.error(err);
+            alert("Something went wrong.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <button
+            onClick={handlePayment}
+            disabled={loading}
+            type="button"
+            className="bg-blue-600 font-semibold text-md px-4 py-2 rounded text-white disabled:bg-gray-400"
+        >
+            {loading ? "Processing..." : "Pay Now"}
+        </button>
+    );
+};
+
+export default PaystackButton;
