a7dc1d2ce31ac669dcbc8fec634fcc1f7e306572
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-22 02:57:43 +0100

    fix: resize contact form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,10 +1,10 @@
 export default function ContactForm() {
     return (
         <>
-            <form className="bg-gray-800 rounded px-10 py-6 w-[70%] md:w-[60%] md:mx-w-[350px] mx-auto">
+            <form className="bg-gray-800 px-4 py-6 w-[100%] md:w-[60%] md:mx-w-[350px] mx-auto">
                 <h2 className="text-center text-white text-2xl font-bold my-3">Contact Form</h2>
                 {/*  */}
-                <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-7 mt-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-4 md:gap-5 mt-10">
                     <div className="input-field">
                         <label htmlFor="email">Email</label>
                         <input type="email" />
@@ -29,7 +29,7 @@ export default function ContactForm() {
                         <textarea className="w-full" id="ct-msg"></textarea>
                     </div>
                 {/*  */}
-                <button className="block w-[40%] text-gray-800 mt-5 bg-white rounded font-semibold text-md px-4 py-2 mx-auto">Submit</button>
+                <button className="block w-[100%] text-gray-800 mt-5 bg-white rounded font-semibold text-md px-4 py-2 mx-auto">Submit</button>
             </form>
     </>)
 }
