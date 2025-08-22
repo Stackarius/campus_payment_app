f91c2a5d9246cb78d8b9ba0d5b3343cd126e7e88
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-22 02:52:31 +0100

    feat: set up api route to query payment with limits and pagination

--- app/api/fetchPayment/route.js
+++ app/api/fetchPayment/route.js
@@ -10,15 +10,42 @@ export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const studentID = searchParams.get("studentID");
 
-  let query = supabase.from("payments").select("*");
+  const from = searchParams.get("from"); // e.g. 2025-01-01
+  const to = searchParams.get("to");
 
+  const page = parseInt(searchParams.get("page") || "1", 10);
+  const limit = parseInt(searchParams.get("limit") || "10", 10);
+  const fromIndex = (page - 1) * limit;
+  const toIndex = fromIndex + limit - 1;
+
+  let query = supabase.from("payments").select("*", { count: "exact" });
+  // filter by student id
   if (studentID) {
     query = query.eq("studentID", studentID);
   }
 
-  const { data, error } = await query;
+  // Filter by date
+  if (from) {
+    query = query.gte("created_at", from);
+  }
+  if (to) {
+    query = query.lte("created_at", to);
+  }
+
+  // Apply Pagination
+  query = query.range(fromIndex, toIndex);
+
+  const { data, error, count } = await query;
 
   if (error) return Response.json({ error: error.message }, { status: 500 });
 
-  return Response.json({ payments: data });
+  return Response.json({
+    payments: data,
+    pagination: {
+      page,
+      limit,
+      total: count,
+      totalPages: Math.ceil(count / limit),
+    },
+  });
 }
