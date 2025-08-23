8a26fb5a88fcb9f088940e248c1bd2a635819fa6
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-23 15:23:57 +0100

    feat: Payment history page, fethh history using student id

---
+++ app/dashboard/paymentHistory/page.jsx
@@ -0,0 +1,100 @@
+'use client'
+
+import { supabase } from "@/lib/supabaseClient";
+import { useEffect, useState } from "react"
+
+export default function PaymentHistory() {
+    const [loading, setLoading] = useState(false)
+    const [page, setPage] = useState(1);
+    const [payments, setPayments] = useState([]);
+    const [pagination, setPagination] = useState({});
+    const [limit] = useState(5);
+    const [ID, setID] = useState('')
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const { data, error } = await supabase.auth.getUser()
+            
+            if (error) {
+                return error.message
+            }
+
+            setID(data.user.user_metadata('student_id'))
+            return
+        }
+
+        async function fetchPayments() {
+            setLoading(true)
+            try {
+                const res = await fetch(`/api/fetchPayment?studentID=${ID}&page=${page}&limit=${limit}`)
+
+                const data = await res.json()
+                setPayments(data.payments)
+                setPagination(data?.pagination)
+            } catch (err) {
+                console.error("Error fetching payments", err);
+            }
+        }
+        fetchData()
+        fetchPayments()
+    }, [page])
+
+    return (
+        <div className="p-6">
+            <h1 className="text-xl font-bold mb-4">Payment History</h1>
+
+            {/* Table */}
+            <table className="w-full border">
+                <thead>
+                    <tr className="bg-gray-100">
+                        <th className="p-2 border">ID</th>
+                        <th className="p-2 border">Student</th>
+                        <th className="p-2 border">Amount</th>
+                        <th className="p-2 border">Type</th>
+                        <th className="p-2 border">Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {loading ? (
+                        <tr><td colSpan="5" className="p-4 text-center">Loading...</td></tr>
+                    ) : payments.length === 0 ? (
+                        <tr><td colSpan="5" className="p-4 text-center">No payments found</td></tr>
+                    ) : (
+                        payments.map((p, index) => (
+                            <tr key={p.id}>
+                                <td className="p-2 border">{index}</td>
+                                <td className="p-2 border">{p.student_id}</td>
+                                <td className="p-2 border">₦{p.amount}</td>
+                                <td className="p-2 border">{p.type}</td>
+                                <td className="p-2 border">{new Date(p.created_at).toLocaleString()}</td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </table>
+
+            {/* Pagination */}
+            <div className="flex justify-between items-center mt-4 ">
+                <button
+                    disabled={page <= 1}
+                    onClick={() => setPage(page - 1)}
+                    className="px-3 py-1 border text-white bg-gray-700 rounded disabled:opacity-50"
+                >
+                    Prev
+                </button>
+                <span>
+                    Page {pagination.page} of {pagination.totalPages || 1}
+                </span>
+                <button
+                    disabled={page >= (pagination.totalPages || 1)}
+                    onClick={() => setPage(page + 1)}
+                    className="px-3 py-1 border text-white bg-gray-700 rounded disabled:opacity-50"
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    )
+}
