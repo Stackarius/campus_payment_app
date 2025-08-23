e6c3355eb520fc58424378c220e0926cc4b9b5fa
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-23 15:21:57 +0100

    feat: added menubutton to toggle sidebar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import Sidebar from "@/components/Sidebar";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function DashboardLayout({ children }) {
     const router = useRouter();
     const [loading, setLoading] = useState(true);
+    const [isOpen, setIsOpen] = useState(false)
 
     useEffect(() => {
         const checkAuth = async () => {
@@ -23,8 +25,14 @@ export default function DashboardLayout({ children }) {
 
     return (
         <div className="flex h-[100vh] overflow-y-hidden">
-            <Sidebar />
+            {isOpen && <Sidebar />}
             <main className="flex-1 h-[100%] overflow-y-scroll bg-gray-100 p-6">{loading ? children : <p>Loading</p>}</main>
+            {/* Menu button */}
+            {!isOpen ?
+                <FaBars size={20} onClick={() => setIsOpen(!isOpen)} className="fixed top-12 right-7 cursor-pointer transition-all duration-300 ease-in-out" />
+                :
+                <FaTimes size={20} onClick={() => setIsOpen(!isOpen)} className="fixed top-12 right-7 cursor-pointer transition-all duration-300 ease-in-out" />
+            }
         </div>
     );
 }
